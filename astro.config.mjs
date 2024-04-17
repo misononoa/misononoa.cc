583193e5763faf929c7fe5a29a9ed1befb872770
misononoa <example@example.com>
2024-04-17 23:18:23 +0900

    iconifyを使う

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://misononoa.cc",
-	integrations: [react()],
+  site: "https://misononoa.cc",
+  integrations: [react(), icon()]
 });
