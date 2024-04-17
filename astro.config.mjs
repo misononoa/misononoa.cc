aa3ae0667844dac7e9c0585b41463a90d7639600
misononoa <example@example.com>
2024-04-17 16:05:23 +0900

    ビルドの出力対象を静的ホストに

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://misononoa.cc",
-  base: "/dist",
-  integrations: [react()]
+	output: "static",
+	site: "https://misononoa.cc",
+	base: "/dist",
+	integrations: [react()],
 });
