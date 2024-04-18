f4b33c8ac3bca8ab260001aed81d0b784e5b3bb9
misononoa <example@example.com>
2024-04-18 20:25:12 +0900

    きれいにしたり絵文字をつけたりした

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,9 +1,43 @@
 import React from "react";
 import "./Navigation.css";
+import { type SVGProps } from "react";
+
+function ExpandButton(props: SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="1em"
+			height="1em"
+			viewBox="0 0 24 24"
+			{...props}
+		>
+			<path
+				fill="currentColor"
+				d="M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6z"
+			></path>
+		</svg>
+	);
+}
+
+function ShrinkButton(props: SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="1em"
+			height="1em"
+			viewBox="0 0 24 24"
+			{...props}
+		>
+			<path
+				fill="currentColor"
+				d="m14 18l-6-6l6-6l1.4 1.4l-4.6 4.6l4.6 4.6z"
+			></path>
+		</svg>
+	);
+}
 
 export interface Props {
 	root?: string;
-	expandButton?: any;
 }
 
 export default function Navigation(props: Props) {
@@ -20,7 +54,11 @@ export default function Navigation(props: Props) {
 	return (
 		<>
 			<div className="toggleButton" onClick={toggleMenu.toggle}>
-				{props.expandButton}
+				{!toggleMenu.isOpen() ? (
+					<ExpandButton fontSize={31} />
+				) : (
+					<ShrinkButton fontSize={31} />
+				)}
 			</div>
 			<div className="nav-links">
 				{toggleMenu.isOpen() && (
