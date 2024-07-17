e0718b67c4ce963b9c287e63d99c327d01aa7ed0
misononoa <misononoa@example.com>
2024-07-17 11:39:45 +0900

    netlify用のセットアップを用意してみる

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://misononoa.cc",
+  output: "server",
+  adapter: netlify(),
+  // site: "https://misononoa.cc",
   integrations: [react(), icon()]
 });
