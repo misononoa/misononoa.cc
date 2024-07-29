89d6d60f875f034f85e644e25aead3c095fc5a2f
misononoa <example@example.com>
2024-07-30 00:44:24 +0900

    フッターやabout_meページのデザイン変更など

---
+++ src/components/toggle-list/ToggleList.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./ToggleList.css";
+
+export interface Props {
+	listname: string;
+	strs: Array<string>;
+}
+
+export default function ToggleList({ listname, strs }: Props): JSX.Element {
+	const toggleButton = (() => {
+		const [state, setState] = React.useState(false);
+		return {
+			isOpen: () => state,
+			toggle: () => setState(!state),
+		};
+	})();
+
+	return (
+		<ul>
+			<summary onClick={toggleButton.toggle}>
+				{!toggleButton.isOpen() ? <>+{listname}</> : <>-{listname}</>}
+			</summary>
+			{toggleButton.isOpen() &&
+				strs.map((s) => (
+					<li className="elements" key={strs.indexOf(s)}>
+						{s}
+					</li>
+				))}
+		</ul>
+	);
+}
