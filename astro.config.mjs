bed5c7efc5131b1f9c6196d2188bdbf52b9b2b19
misononoa <example@example.com>
2024-09-26 19:19:44 +0900

    cloudflare pagesに移行する

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: cloudflare(),
   // site: "https://misononoa.cc",
   integrations: [react(), icon()]
 });
