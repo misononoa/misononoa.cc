4038a7f9db587af14ba830662af7604db1f5951b
misononoa <misononoa@example.com>
2024-09-27 11:55:27 +0900

    favicon追加とかデザインの変更とか

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -4,13 +4,7 @@ export interface Props {
 	messages?: Array<string>;
 }
 
-export default function Message(props: Props): JSX.Element {
-	const messages: Array<string> = [
-		"こんにちは！",
-		"トマトが好きです",
-		"口の中の皮を噛む癖があります",
-		"元気",
-	].concat(props.messages);
+export default function Message({ messages }: Props): JSX.Element {
 	const [text, setText] = React.useState("");
 	React.useEffect(() => {
 		const index = Math.floor(Math.random() * (messages.length - 1));
