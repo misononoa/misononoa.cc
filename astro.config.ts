f9797e6c5053f68c88536fe18fae78a766c8ccaf
misononoa <me@misononoa.cc>
2026-07-29 01:19:04 +0900

    バージョン上げて、非推奨に対応して、mdx入れて、yumemaのドキュメントをつけたりとか

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,12 @@ import vue from "@astrojs/vue";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     site: "https://misononoa.cc",
-    integrations: [vue(), icon()],
+    integrations: [vue(), icon(), mdx()],
     adapter: vercel(),
 });
